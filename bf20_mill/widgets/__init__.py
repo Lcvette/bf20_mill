7e2a9c6dadd2c86857ac4e322cba5d77c9e2e358
TurBoss <j.l.toledano.l@gmail.com>
2023-03-11 22:37:05 +0100

    fix modules path in the __init__.py file

--- bf20_mill/widgets/__init__.py
+++ bf20_mill/widgets/__init__.py
@@ -1,10 +1,10 @@
 from qtpyvcp.widgets.qtdesigner import _DesignerPlugin
 
-from widgets.conversational.facing import FacingWidget
-from widgets.conversational.xy_coord import XYCoordWidget
-from widgets.conversational.hole_circle import HoleCircleWidget
-from widgets.conversational.int_line_edit import IntLineEdit
-from widgets.conversational.float_line_edit import FloatLineEdit
+from bf20_mill.widgets.conversational.facing import FacingWidget
+from bf20_mill.widgets.conversational.xy_coord import XYCoordWidget
+from bf20_mill.widgets.conversational.hole_circle import HoleCircleWidget
+from bf20_mill.widgets.conversational.int_line_edit import IntLineEdit
+from bf20_mill.widgets.conversational.float_line_edit import FloatLineEdit
 
 
 
